c003e093a6e53591e4a223e41e4b034ce5745d92
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-06-01 22:42:48 +0530

    Create armstrongNumber_gfg.java

---
+++ armstrongNumber_gfg.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+class Solution {
+    static boolean armstrongNumber(int n) {
+       int temp=n;
+       int sum=0;
+       while(n>0){
+           int ld=n%10;
+           sum=sum+(ld*ld*ld);
+           n=n/10;
+       }
+       if(sum==temp) return true;
+       else return false;
+        
+    }
+}
