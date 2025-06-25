a5c5e1f0c59ae8c9bb6c6b55684e77378656c884
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-06-25 15:54:12 +0530

    Update selection_sort_gfg.java

--- selection_sort_gfg.java
+++ selection_sort_gfg.java
@@ -1,9 +1,9 @@
 class Solution {
     void selectionSort(int[] arr) {
         // code here
-        for(int i=0;i<=arr.length-2;i++){
+        for(int i=0;i<arr.length-1;i++){
             int minpos=i;
-            for(int j=i;j<=arr.length-1;j++){
+            for(int j=i;j<arr.length;j++){
                 if(arr[minpos]>arr[j]){
                     minpos=j;
                 }
